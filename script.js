fb692b6c3a23d32cc2d35ff917332f6c0855b042
NicolasArcelin <103464938+NicolasArcelin@users.noreply.github.com>
2022-07-22 18:27:04 -0400

    + Adaptive UI & Mouse Hover Color Changes Size of grid is now corrilated to the users input. Hover mouse over the grid will change div background color of div.
    "TEST" button is inert.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+let sizeInput = 16;
+
 //Creates a sketch pad 
+
 function divAdd () {
     const gridContainer = document.querySelector('.gridContainer');
     const newDiv = document.createElement('div');
@@ -6,8 +9,53 @@ function divAdd () {
     gridContainer.appendChild(newDiv);
 }
 
-for (let i = 0; i < 256; i++) {
-    divAdd();
+function divRemove () {
+    const gridContainer = document.querySelector('.gridContainer');
+    let gridLastChild = gridContainer.lastElementChild;
+    while (gridLastChild) {
+        gridContainer.removeChild(gridLastChild);
+        gridLastChild = gridContainer.lastElementChild;
+    }
 };
 
+//Hover detail below
+let hoverColor = () => {
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => {
+            grid.addEventListener('mouseover', function () {
+            grid.style.setProperty('background-color', 'black')
+        });
+            grid.addEventListener('mouseout', function () {
+            grid.style.setProperty('background-color', 'white')
+        });
+    })
+};
 
+let sizeLoop = () => {
+    divRemove();
+    for (let i = 0; i < (sizeInput*sizeInput); i++) {
+    divAdd();
+    console.log('looped');
+    }
+    hoverColor();
+};
+
+sizeLoop();
+
+
+
+//Change size of drawing pad
+
+const input = document.getElementById('gridSizeInput');
+
+input.addEventListener('change', (e) => {
+    sizeInput = input.value;
+// console.log((sizeInput));
+    root.style.setProperty('--size', sizeInput);
+    sizeLoop();
+});
+
+const root = document.querySelector(':root');
+const rootStyles = getComputedStyle(root);
+const gridSize = rootStyles.getPropertyValue('--size')
+// console.log(gridSize);
